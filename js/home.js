0635d4c93577422b204c700f5b2463bc54497899
pdpscarface <patrick.zhou89@gmail.com>
2017-06-13 18:41:17 -0400

    Initial website code commit

---
+++ js/home.js
@@ -0,0 +1,17 @@
+(function(window, document, $, undefined){
+	$('#mobile-menu-icon').on('click',function(){
+		$(this).toggleClass('toggle-menu');
+		$('#header').slideToggle();
+	});
+	$("a").on('click',function(e){
+		if(this.hash !==""){
+			e.preventDefault();
+		}
+		var hash=this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		},400, function(){
+			window.location.hash=hash;
+		});
+	});
+})(window, document, $);
